a8cbd3b8ae1297e836e8409f06433a6d13e97868
Wonjae98 <dnjswo980213@gmail.com>
2022-07-28 16:31:32 +0900

    member repository + test case
    spring bean dependencies

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,48 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository=memberRepository;
+    }
+
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원x
+        validateDuplicateMember(member);
+        //Optional 로 감싼 다음에 반환해주므로 그 안에 method 활용가능 member 객체가 있는 것임.
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다. ");
+        });
+    }
+
+    /**
+     *
+     * 전체 회원 조회
+     */
+    public List<Member> findMember(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+}
